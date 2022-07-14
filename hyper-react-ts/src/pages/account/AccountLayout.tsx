04f29a259b3a25dce707937b28238fbe43da998a
dyi919 <ehddjs13579@gmail.com>
2022-07-14 13:10:34 +0900

    [Style] Primary color changed

--- hyper-react-ts/src/pages/account/AccountLayout.tsx
+++ hyper-react-ts/src/pages/account/AccountLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import Logo from 'assets/images/asyncrum-logo-cool.png';
+import Logo from 'assets/images/asyncrum-logo-white.png';
 import { useAccountLayout } from './hooks';
 
 type AccountLayoutProps = {
@@ -26,7 +26,7 @@ const AccountLayout = ({ bottomLinks, children }: AccountLayoutProps) => {
                                 <Card.Header className="pt-4 pb-4 text-center bg-primary">
                                     <Link to="/">
                                         <span>
-                                            <img src={Logo} alt="" height="42" />
+                                            <img src={Logo} alt="" height="100" />
                                         </span>
                                     </Link>
                                 </Card.Header>
