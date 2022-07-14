5084d1751652254c0762c9a6c21af9e84392bf00
dyi919 <ehddjs13579@gmail.com>
2022-07-14 12:23:11 +0900

    [Feature] Google, Naver buttons in login page implemented

--- hyper-react-ts/src/pages/account/Login.tsx
+++ hyper-react-ts/src/pages/account/Login.tsx
@@ -4,6 +4,8 @@ import { useTranslation } from 'react-i18next';
 import { VerticalForm, FormInput } from 'components';
 import AccountLayout from './AccountLayout';
 import { useLogin } from './hooks';
+import naverIcon from 'assets/images/btnW_icon_circle.png'
+import googleIcon from 'assets/images/google.png'
 
 export type UserData = {
     username: string;
@@ -38,10 +40,7 @@ const Login = () => {
 
             <AccountLayout bottomLinks={<BottomLink />}>
                 <div className="text-center w-75 m-auto">
-                    <h4 className="text-dark-50 text-center mt-0 fw-bold">{t('Sign In')}</h4>
-                    <p className="text-muted mb-4">
-                        {t('Enter your email address and password to access admin panel.')}
-                    </p>
+                    <h4 className="text-dark-50 text-center mt-0 mb-4 fw-bold">{t('Sign In')}</h4>
                 </div>
 
                 {error && (
@@ -55,28 +54,12 @@ const Login = () => {
                     resolver={schemaResolver}
                     defaultValues={{ username: 'test', password: 'test' }}
                 >
-                    <FormInput
-                        label={t('Username')}
-                        type="text"
-                        name="username"
-                        placeholder={t('Enter your Username')}
-                        containerClass={'mb-3'}
-                    />
-                    <FormInput
-                        label={t('Password')}
-                        type="password"
-                        name="password"
-                        placeholder={t('Enter your password')}
-                        containerClass={'mb-3'}
-                    >
-                        <Link to="/account/forget-password" className="text-muted float-end">
-                            <small>{t('Forgot your password?')}</small>
-                        </Link>
-                    </FormInput>
-
                     <div className="mb-3 mb-0 text-center">
-                        <Button variant="primary" type="submit" disabled={loading}>
-                            {t('Log In')}
+                        <Button className="signin-button" variant="primary" type="submit" disabled={loading}>
+                            <img className="signin-icon" src={naverIcon} alt="Naver Icon" /> Sign in with Naver
+                        </Button>
+                        <Button className="signin-button" variant="primary" type="submit" disabled={loading}>
+                            <img className="signin-icon" src={googleIcon} alt="Google Icon" /> Sign in with Google
                         </Button>
                     </div>
                 </VerticalForm>
