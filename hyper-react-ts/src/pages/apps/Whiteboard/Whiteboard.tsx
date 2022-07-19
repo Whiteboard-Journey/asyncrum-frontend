4f66bee800851a8a7348bb250297f64bf4d4716b
dyi919 <ehddjs13579@gmail.com>
2022-07-19 17:26:58 +0900

    [Fix] Prev button removed

--- hyper-react-ts/src/pages/apps/Whiteboard/Whiteboard.tsx
+++ hyper-react-ts/src/pages/apps/Whiteboard/Whiteboard.tsx
@@ -1,10 +1,8 @@
 import { Tldraw } from "@tldraw/tldraw"
-import { Button } from "react-bootstrap";
 
 const Whiteboard = () => {
     return (
     <div>
-        <Button className="whiteboard-prev-button" href="dashboard/ecommerce" variant="primary" />
         <Tldraw />
     </div>);
 }
