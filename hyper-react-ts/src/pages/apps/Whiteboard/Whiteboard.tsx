a135cecdbcc90d2ffeb0d2962002381eb32ed8e1
dyi919 <ehddjs13579@gmail.com>
2022-07-19 17:17:56 +0900

    [Feature] tldraw added to whiteboard page

--- hyper-react-ts/src/pages/apps/Whiteboard/Whiteboard.tsx
+++ hyper-react-ts/src/pages/apps/Whiteboard/Whiteboard.tsx
@@ -1,5 +1,12 @@
+import { Tldraw } from "@tldraw/tldraw"
+import { Button } from "react-bootstrap";
+
 const Whiteboard = () => {
-    return (<div>Whiteboard</div>);
+    return (
+    <div>
+        <Button className="whiteboard-prev-button" href="dashboard/ecommerce" variant="primary" />
+        <Tldraw />
+    </div>);
 }
 
 export { Whiteboard };
