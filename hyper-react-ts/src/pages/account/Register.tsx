2c6f3db83f66a140e907fe845206b4621c2f47ba
dyi919 <ehddjs13579@gmail.com>
2022-07-23 17:15:12 +0900

    [Feature] OAuth signin buttons activated

--- hyper-react-ts/src/pages/account/Register.tsx
+++ hyper-react-ts/src/pages/account/Register.tsx
@@ -6,6 +6,7 @@ import AccountLayout from './AccountLayout';
 import { useRegister } from './hooks';
 import naverIcon from 'assets/images/btnW_icon_circle.png'
 import googleIcon from 'assets/images/google.png'
+import config from 'config';
 
 export type UserData = {
     fullname: string;
@@ -88,11 +89,11 @@ const Register = () => {
                     </div>
 
                     <div className="pt-3 mb-3 mb-0 text-center border-top">
-                        <Button className="signin-button" variant="primary" type="submit" disabled={loading}>
-                            <img className="signin-icon" src={naverIcon} alt="Naver Icon" /> Sign up with Naver
+                        <Button href={config.API_URL+"/oauth2/authorization/naver?redirect_uri=http://localhost:3000/account/oauth"}className="signin-button" variant="primary" type="submit" disabled={loading}>
+                            <img className="signin-icon" src={naverIcon} alt="Naver Icon" /> Sign in with Naver
                         </Button>
-                        <Button className="signin-button" variant="primary" type="submit" disabled={loading}>
-                            <img className="signin-icon" src={googleIcon} alt="Google Icon" /> Sign up with Google
+                        <Button href={config.API_URL+"/oauth2/authorization/google?redirect_uri=http://localhost:3000/account/oauth"} className="signin-button" variant="primary" type="submit" disabled={loading}>
+                            <img className="signin-icon" src={googleIcon} alt="Google Icon" /> Sign in with Google
                         </Button>
                     </div>
                 </VerticalForm>
