4a5dc66b0de236e1131a8970fe566550b3595f3e
dyi919 <ehddjs13579@gmail.com>
2022-08-17 12:56:35 +0900

    WBJ-410 Settings profile pic/team logo hover message created

--- hyper-react-ts/src/pages/settings/TeamSettings.tsx
+++ hyper-react-ts/src/pages/settings/TeamSettings.tsx
@@ -197,16 +197,21 @@ const TeamSettings = () => {
                                     <Col md={{ span: 3, offset: 2 }}>
                                         <div style={{ height: 190, position: "relative" }}>
                                             <p className='mb-1' style={{ fontWeight: '600' }}>Team Logo</p>
-                                            <img src={previewImage} alt="profile preview" className="rounded ratio ratio-1x1" style={{ position: "absolute", width: 150, height: 150, cursor: "pointer" }} onClick={()=>{fileInput.current!.click()}} referrerPolicy="no-referrer" />
-                                            <input 
-                                                type='file' 
-                                                // accept='image/jpg, image/png, image/jpeg' 
-                                                accept='image/png'
-                                                style={{display:'none'}}
-                                                name='logoImage'
-                                                onChange={onChange}
-                                                ref={fileInput}
-                                            />
+                                            <div className='container'>
+                                                <img src={previewImage} alt="profile preview" className="rounded ratio ratio-1x1" style={{ position: "absolute", width: 150, height: 150, cursor: "pointer" }} referrerPolicy="no-referrer" />
+                                                <input 
+                                                    type='file' 
+                                                    // accept='image/jpg, image/png, image/jpeg' 
+                                                    accept='image/png'
+                                                    style={{display:'none'}}
+                                                    name='logoImage'
+                                                    onChange={onChange}
+                                                    ref={fileInput}
+                                                />
+                                                <div className='overlay rounded' onClick={()=>{fileInput.current!.click()}}>
+                                                    <div className='text'>click to upload</div>
+                                                </div>
+                                            </div>
                                         </div>
                                         <Button className="me-2" onClick={onSaveLogoImage} >
                                             Save
