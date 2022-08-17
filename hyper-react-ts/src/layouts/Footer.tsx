ce2b15c6639abec579cec7cfdc4baa3f4454cdbf
dyi919 <ehddjs13579@gmail.com>
2022-08-17 09:05:50 +0900

    WBJ-405 footer content changed

--- hyper-react-ts/src/layouts/Footer.tsx
+++ hyper-react-ts/src/layouts/Footer.tsx
@@ -2,12 +2,11 @@ import { Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
     return (
         <footer className="footer">
             <div className="container-fluid">
                 <Row>
-                    <Col md={6}>{currentYear} © Hyper - Coderthemes.com</Col>
+                    <Col md={6}>2022 © Asyncrum - Software Maestro 13th Team WBJ</Col>
 
                     <Col md={6}>
                         <div className="text-md-end footer-links d-none d-md-block">
