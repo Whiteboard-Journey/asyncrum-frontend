71876298b53f9b31d08615b65f657136795b2cb4
dyi919 <ehddjs13579@gmail.com>
2022-08-30 15:42:16 +0900

    [refactor] Removed unnecessary packages

---
+++ src/components/index.tsx
@@ -0,0 +1,9 @@
+export * from './form';
+export * from './ThemeCustomizer';
+export * from './FAQs';
+export * from './PricingCard';
+export { default as Loader } from './Loader';
+export { default as MessageItem } from './MessageItem';
+export { default as MessageList } from './MessageList';
+export { default as PageTitle } from './PageTitle';
+export { default as VideoRecorder } from './VideoRecorder';
