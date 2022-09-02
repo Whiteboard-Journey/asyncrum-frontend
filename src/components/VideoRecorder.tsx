352374b0f3727d6520957144dcf4c6d59e5c0274
dyi919 <ehddjs13579@gmail.com>
2022-09-02 23:14:04 +0900

    [refactor] unnecessary comments deleted

--- src/components/VideoRecorder.tsx
+++ src/components/VideoRecorder.tsx
@@ -9,61 +9,6 @@ const cam_w = 320,
   screen_h = 540;
   const { recordingState, webcamRef, videoRef, previewStream, camMediaBlobUrl, screenMediaBlobUrl, setRecordingState, camStartRecording, camStopRecording, screenStartRecording, screenStopRecording, uploadVideoes } = useVideoRecorder();
 
-
-  // const user = JSON.parse(sessionStorage.getItem('asyncrum_user')!);
-  // const title = user.fullname + ' ' + Date.now();
-  // const description = 'Daily standups - ' + title;
-  // const videoRef = useRef<HTMLVideoElement>(null);
-  // const webcamRef = useRef<Webcam>(null);
-  // const [recordingState, setRecordingState] = useState<'idle' | 'recording' | 'recorded'>('idle');
-
-  // const {
-  //   status: camStatus,
-  //   startRecording: camStartRecording,
-  //   stopRecording: camStopRecording,
-  //   mediaBlobUrl: camMediaBlobUrl,
-  //   previewStream,
-  // } = useReactMediaRecorder({ video: true });
-
-  // const {
-  //   startRecording: screenStartRecording,
-  //   stopRecording: screenStopRecording,
-  //   mediaBlobUrl: screenMediaBlobUrl,
-  // } = useReactMediaRecorder({ video: true, screen: true });
-
-  // const uploadVideo = async (url: string, type: string) => {
-  //   const camMedia = await fetch(url);
-  //   const blob = await camMedia.blob();
-  //   const body = {
-  //     title: title + ' ' + type,
-  //     description: description + ' ' + type,
-  //     scope: 'team',
-  //   };
-
-  //   const createRecordAPIResponse = await createRecordAPI(body);
-  //   const presignedURL = createRecordAPIResponse.data.preSignedURL;
-  //   const fileToUpload = new File([blob], title + ' ' + type + '.mp4', { type: 'video/mp4' });
-  //   await uploadRecordAPI(presignedURL, fileToUpload);
-  // };
-
-  // const uploadVideoes = async () => {
-  //   if (!camMediaBlobUrl || !screenMediaBlobUrl) {
-  //     alert('Recordings are not ready');
-  //     return;
-  //   }
-
-  //   await uploadVideo(camMediaBlobUrl, 'cam');
-  //   await uploadVideo(screenMediaBlobUrl, 'screen');
-
-  //   window.location.reload();
-  // };
-
-  // useEffect(() => {
-  //   if (videoRef.current && previewStream) {
-  //     videoRef.current.srcObject = previewStream;
-  //   }
-  // }, [previewStream]);
-
   return (
     <div className="video-recorder">
       <div>
