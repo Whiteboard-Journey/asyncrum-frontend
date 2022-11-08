2a6ec22e726974bfbdb81c15912c66d9581639eb
dyi919 <ehddjs13579@gmail.com>
2022-11-08 21:17:01 +0900

    [feature] WBJ-946 User timezone setting added

--- src/pages/settings/PersonalInfoForm.tsx
+++ src/pages/settings/PersonalInfoForm.tsx
@@ -1,10 +1,23 @@
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
+import TimezoneSelect from 'react-timezone-select';
 import { FormInput } from 'components';
 import { usePersonalSettings } from './hooks';
+import { useState } from 'react';
+import { useRedux } from 'hooks';
 
 const PersonalInfoForm: React.FC = () => {
-  const { userFullname, onSubmitProfileInfo } = usePersonalSettings();
+  const { appSelector } = useRedux();
 
+  const { user } = appSelector((state) => ({
+    user: state.Auth.user,
+  }));
+
+  const { userFullname, onSubmitProfileInfo } = usePersonalSettings();
+  const [selectedTimezone, setSelectedTimezone] = useState(
+    user.timeZone
+      ? { value: user.timeZone, label: '' }
+      : { value: Intl.DateTimeFormat().resolvedOptions().timeZone, label: '' }
+  );
   return (
     <form onSubmit={onSubmitProfileInfo}>
       <FormInput
@@ -14,10 +27,14 @@ const PersonalInfoForm: React.FC = () => {
         containerClass={'mb-3'}
         key="fullname"
         placeholder={userFullname}
-        required
       />
       <FormInput label="Company Name" type="text" name="companyName" containerClass={'mb-3'} key="companyName" />
       <FormInput label="Role" type="text" name="role" containerClass={'mb-3'} key="role" />
+      <Form.Label htmlFor="timezone">Timezone</Form.Label>
+      <Form.Control type="hidden" id="timezone" aria-describedby="timezone" value={selectedTimezone.value} />
+      <div className="mb-3">
+        <TimezoneSelect value={selectedTimezone.value} onChange={setSelectedTimezone} />
+      </div>
       <Button color="primary" type="submit">
         Save Changes
       </Button>
