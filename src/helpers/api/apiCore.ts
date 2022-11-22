0a86968a66d8051fac02bb8fa85af8e44886f7db
dyi919 <ehddjs13579@gmail.com>
2022-11-22 21:54:47 +0900

    [hotfix] Removed axios withCredentials option

--- src/helpers/api/apiCore.ts
+++ src/helpers/api/apiCore.ts
@@ -5,7 +5,6 @@ import config from '../../config';
 // content type
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.baseURL = config.API_URL;
-axios.defaults.withCredentials = true;
 
 // intercepting to capture errors
 axios.interceptors.response.use(
